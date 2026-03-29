1eb4fcf6c98e224f75e861d0da6e2f2f56c47b16
drsalmaelnour-svg <dr.salmaelnour@gmu.ac.ae>
2026-03-29 18:29:17 +0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,6 @@
  * Unauthorised copying, modification, or distribution is strictly prohibited.
  * See LICENSE for full terms.
  */
-```
-5. Scroll down, set the commit message to:
-```
-chore: add copyright header to main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
