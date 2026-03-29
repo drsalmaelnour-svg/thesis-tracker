0b4f60409f238b87749872622d2310ec408fe53d
drsalmaelnour-svg <dr.salmaelnour@gmu.ac.ae>
2026-03-29 18:25:17 +0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,15 @@
+/**
+ * Thesis Coordination System
+ * © 2025 Dr Salma Elnour. All rights reserved.
+ *
+ * This file is part of proprietary software owned by Dr Salma Elnour.
+ * Unauthorised copying, modification, or distribution is strictly prohibited.
+ * See LICENSE for full terms.
+ */
+```
+5. Scroll down, set the commit message to:
+```
+chore: add copyright header to main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
